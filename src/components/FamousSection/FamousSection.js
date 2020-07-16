bd2443021d7689783dd6c66a7b518b94bbd7ab11
Kevin Anderson <kmichael419@hotmail.com>
2020-07-16 10:34:16 -0500

    adding famous people to the DOM upon input

--- src/components/FamousSection/FamousSection.js
+++ src/components/FamousSection/FamousSection.js
@@ -2,44 +2,66 @@ import React, { Component } from 'react';
 import './FamousSection.css';
 
 class FamousSection extends Component {
-
   state = {
     famousPerson: {
       name: '',
       role: '',
-    }
-  }
+    },
+    famousPersonList: [],
+  };
 
   handleChangeFor = (event, propertyName) => {
     this.setState({
       famousPerson: {
         ...this.state.famousPerson,
         [propertyName]: event.target.value,
-      }
-    })
-  }
+      },
+    });
+  };
 
   addPerson = (event) => {
     event.preventDefault();
-    console.log( `The famous person is `, this.state.famousPerson );
-  }
+    console.log(`The famous person is `, this.state.famousPerson);
+    this.setState({
+      famousPersonList: [
+        ...this.state.famousPersonList,
+        this.state.famousPerson, //entered employee
+      ],
+    });
+  };
 
   render() {
+    const displayedFamousPeople = this.state.famousPersonList.map(
+      (item, index) => {
+        return (
+          <div key={index}>
+            <h4>{item.name}</h4>
+            <p>Role: {item.role}</p>
+          </div>
+        );
+      }
+    );
+
     return (
       <section className="new-person-section">
         <form onSubmit={this.addPerson}>
           <label htmlFor="name-input">Name:</label>
-          <input id="name-input" onChange={(event) => this.handleChangeFor(event, 'name')} />
+          <input
+            id="name-input"
+            onChange={(event) => this.handleChangeFor(event, 'name')}
+          />
           <label htmlFor="role-input">Famous for:</label>
-          <input id="role-input" onChange={(event) => this.handleChangeFor(event, 'role')} />
+          <input
+            id="role-input"
+            onChange={(event) => this.handleChangeFor(event, 'role')}
+          />
           <button type="submit">Done</button>
         </form>
         <p>
-          {this.state.famousPerson.name} is famous for "{this.state.famousPerson.role}".
+          {this.state.famousPerson.name} is famous for "
+          {this.state.famousPerson.role}".
         </p>
-        <ul>
-          {/* The list should go here. */}
-        </ul>
+        <ul>{displayedFamousPeople}</ul>
       </section>
     );
   }
